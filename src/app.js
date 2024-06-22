5deef8ae9f6f4af979f5396bfed2863a9516d3c5
sonu4312 <sonu.kushwaha9872@gmail.com>
2024-06-22 14:48:01 +0530

    set up for user controller and route

--- src/app.js
+++ src/app.js
@@ -17,4 +17,10 @@ app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
 
+//routes import
+
+import userRoutes from "./routes/user.routes.js";
+
+app.use("/api/v1/user", userRoutes);
+
 export { app };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 import connectDB from "./db/connections.js";
 import dotenv from "dotenv";
 dotenv.config();
+import { app } from "./app.js";
 
-connectDB();
+const port = process.env.PORT || 6000;
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running at port : ${port}`);
+    });
+  })
+  .catch((er) => {
+    console.log("Mongo db connection error ", er);
+  });
