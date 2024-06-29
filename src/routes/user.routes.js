768ace8955b87133a4023632076f46fef8c69605
sonu4312 <sonu.kushwaha9872@gmail.com>
2024-06-29 18:26:09 +0530

    Routes has been added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,9 +1,16 @@
 import { Router } from "express";
 import {
+  changePassword,
+  getCurrentUser,
+  getWatchHistory,
   loginUser,
   logoutUser,
   refreshAccessToken,
   registerUser,
+  updateAccDetails,
+  updateAvatar,
+  updateCoverImg,
+  userChannelProfile,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../middlewares/auth.middleware.js";
@@ -20,5 +27,18 @@ router.route("/register").post(
 
 router.route("/login").post(loginUser);
 router.route("/logout").post(verifyToken, logoutUser);
-router.post("/refresh-toke").post(refreshAccessToken);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(verifyToken, changePassword);
+router.route("/current-user").get(verifyToken, getCurrentUser);
+router.route("/update-profile").patch(verifyToken, updateAccDetails);
+router
+  .route("/avatar")
+  .patch(verifyToken, upload.single("avatar"), updateAvatar);
+router
+  .route("/coverImage")
+  .patch(verifyToken, upload.single("coverImg"), updateCoverImg);
+
+router.route("/channel/:username").get(verifyToken, userChannelProfile);
+
+router.get("/history").get(verifyToken, getWatchHistory);
 export default router;
